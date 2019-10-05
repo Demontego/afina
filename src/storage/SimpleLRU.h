588dde2292175a0a4ad9f99727b075bd6c77ebdf
Rinat <Demontego>
2019-10-05 21:05:25 +0300

    update

--- src/storage/SimpleLRU.h
+++ src/storage/SimpleLRU.h
@@ -1,12 +1,12 @@
 #ifndef AFINA_STORAGE_SIMPLE_LRU_H
 #define AFINA_STORAGE_SIMPLE_LRU_H
 
+#include <iostream>
 #include <map>
 #include <memory>
 #include <mutex>
 #include <string>
 #include <utility>
-#include <iostream>
 
 #include <afina/Storage.h>
 
@@ -19,23 +19,18 @@ namespace Backend {
  */
 class SimpleLRU : public Afina::Storage {
 public:
-    SimpleLRU(size_t max_size = 1024) :
-	_max_size(max_size),_current_size(0), _lru_head(nullptr),_lru_tail(nullptr) {}
+    SimpleLRU(size_t max_size = 1024) : _max_size(max_size), _current_size(0), _lru_head(nullptr), _lru_tail(nullptr) {}
 
     ~SimpleLRU() {
-		if (_lru_head == nullptr) {
-		return;
-	}
-	_lru_index.clear();
-
-
-	while(_lru_head->next != nullptr )
-	{
-		std::unique_ptr<lru_node> node_to_delete;
-		node_to_delete.swap(_lru_head);
-		_lru_head.swap(node_to_delete->next);
-	}
-	_lru_head.reset();
+        if (_lru_head) {
+            _lru_index.clear();
+            while (_lru_head->next != nullptr) {
+                std::unique_ptr<lru_node> node_to_delete;
+                node_to_delete.swap(_lru_head);
+                _lru_head.swap(node_to_delete->next);
+            }
+            _lru_head.reset();
+        }
     }
 
     // Implements Afina::Storage interface
@@ -60,10 +55,10 @@ private:
         std::string value;
         lru_node *prev;
         std::unique_ptr<lru_node> next;
-		lru_node (const std::string &k,const std::string &v)
-        :key(k), value(v), prev(nullptr),next(nullptr){}
+        lru_node(const std::string &k, const std::string &v) : key(k), value(v), prev(nullptr), next(nullptr) {}
     };
-
+    using mapT = std::map<std::reference_wrapper<const std::string>, std::reference_wrapper<lru_node>,
+                          std::less<const std::string>>;
     // Maximum number of bytes could be stored in this cache.
     // i.e all (keys+values) must be less the _max_size
     std::size_t _max_size, _current_size;
@@ -73,12 +68,12 @@ private:
     //
     // List owns all nodes
     std::unique_ptr<lru_node> _lru_head;
-    lru_node* _lru_tail;
+    lru_node *_lru_tail;
     // Index of nodes from list above, allows fast random access to elements by lru_node#key
-    std::map<std::string,std::reference_wrapper<lru_node> > _lru_index;
+    mapT _lru_index;
 
     bool put_node(const std::string &key, const std::string &value);
-    bool update_node(const std::string &key,const std::string &value);
+    bool update_node(const mapT::iterator &it, const std::string &value);
     bool remove_node(lru_node &delete_node);
     bool move_node_tail(lru_node &node);
 };
